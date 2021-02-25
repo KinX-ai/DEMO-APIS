6e9edfcbd5b62763b8ef66ace18958aeaa815993
BerVerCoder <justonly.maihuybao@gmail.com>
2021-02-25 23:41:50 +0700

    hey

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ var meme = JSON.parse(fs.readFileSync("./meme.json","utf-8"));
 var jimmy = JSON.parse(fs.readFileSync("./jimmy.json","utf-8"));
 var duckbo = JSON.parse(fs.readFileSync("./duckbo.json","utf-8"));
 var gai = JSON.parse(fs.readFileSync("./gai.json","utf-8"));
+var trai = JSON.parse(fs.readFileSync("./trai.json","utf-8"));
 app.set("port", process.env.PORT || 4000);
-
+var author = "TEAM AUTHOR: \nMai Huy Bảo, \nNguyễn Thế Nam, \nNH Tiến Lâm, \nMinh Mẫn, \nĐặng Văn Hùng"
+var contact = "CONTACT AT:\nhttps://www.facebook.com/JustOnly.MaiHuyBao.Unofficial\nhttps://www.facebook.com/NNam13\nhttps://www.facebook.com/tienlam.nh.9\nhttps://www.facebook.com/MinhMan.4\nhttps://www.facebook.com/hungchodz99"
 app.get('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":"Bảo nói với bạn rằng :Nhìn cái beep","author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = `LIST API:\n• https://api.berver.tech/meme\nAPI meme việt\n• https://api.berver.tech/cosplay\nAPI ảnh cosplay\n• https://api.berver.tech/lyrics/title/artist\nAPI lời bài hát\n• https://api.berver.tech/cadao\nAPI ca dao\n• https://api.berver.tech/avatar/id=\nAPI lấy avatar Facebook không cần token\n• https://api.berver.tech/duckbo\nAPI ảnh Trần Đức Bo\n• https://api.berver.tech/jimmy\nAPI ảnh Jimmy Nguyễn\n• https://api.berver.tech/sim/[text]\nAPI simsimi Tiếng Việt\n• https://api.berver.tech/simteach/[hoi]/[dap]\nAPI dạy simsimi nói\n• https://api.berver.tech/gai\nAPI ảnh gái\n• https://api.berver.tech/trai\nAPI ảnh trai\n\n\n${author}\n\n\n${contact}`
    console.log('/',req.ip);
-   res.end(JSON.stringify(response));
+   res.end(response);
 })
 
 app.get('/cadao', async function (req, res) {
@@ -23,7 +25,7 @@ app.get('/cadao', async function (req, res) {
   var random = await Math.floor(Math.random() * lenght) + 1
   var cadaorq = await cadao.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data": `${cadaorq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data": `${cadaorq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/cadao',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -32,7 +34,7 @@ app.get('/cosplay', async function (req, res) {
   var random = await Math.floor(Math.random() * length) + 1;
   var cosplayrq = await cosplay.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data": `${cosplayrq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data": `${cosplayrq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/cosplay',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -41,7 +43,7 @@ app.get('/meme', async function (req, res) {
   var random = await Math.floor(Math.random() * lenght)
   var memerq = await meme.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":`${memerq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data":`${memerq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/meme',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -57,7 +59,7 @@ app.get('/jimmy', async function (req, res) {
   var random = await Math.floor(Math.random() * lenght)
   var jimmyrq = await jimmy.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":`${jimmyrq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data":`${jimmyrq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/jimmy',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -65,7 +67,7 @@ app.get('/lyrics/:title/:artist', async function (req, res) {
 const lyricsFinder = require('lyrics-finder');
     let lyrics = await lyricsFinder(req.params.artist, req.params.title) || "Not Found!";
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":`${lyrics}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data":`${lyrics}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/lyrics',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -74,7 +76,7 @@ app.get('/duckbo', async function (req, res) {
   var random = await Math.floor(Math.random() * lenght)
   var duckborq = await duckbo.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":`${duckborq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data":`${duckborq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/duckbo',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -83,7 +85,7 @@ app.get('/gai', async function (req, res) {
   var random = await Math.floor(Math.random() * lenght)
   var gairq = await gai.data[String(random)]
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = {"data":`${gairq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+   var response = {"data":`${gairq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/gai',req.ip)
    res.end(JSON.stringify(response));
 })
@@ -100,7 +102,7 @@ var options = {
 };
 request(options, function (error, rp) {
   if (error) throw new Error(error);
- var response = {"data":`${JSON.parse(rp.body).msg}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+ var response = {"data":`${JSON.parse(rp.body).msg}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/sim/',req.params.text,rp.body.msg,req.ip);
    res.end(JSON.stringify(response));});
 })
@@ -116,9 +118,18 @@ var options = {
 };
 request(options, function (error, rp) {
   if (error) throw new Error(error);
- var response = {"data":`Simsimi đã tiếp thu câu trả lời của bạn`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99","Homepage":"berver.tech"}
+ var response = {"data":`Simsimi đã tiếp thu câu trả lời của bạn`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
    console.log('/sim/',req.params.text,rp.body.msg,req.ip);
    res.end(JSON.stringify(response));});
+})
+app.get('/trai', async function (req, res) {
+  var lenght = Number(trai.lenght);
+  var random = await Math.floor(Math.random() * lenght)
+  var trairq = await trai.data[String(random)]
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   var response = {"data":`${trairq}`,"author":"Mai Huy Bảo, Nguyễn Thế Nam, NH Tiến Lâm, Đặng Văn Hùng, Minh Mẫn","contact":"https://www.facebook.com/JustOnly.MaiHuyBao.Unofficial, https://www.facebook.com/NNam13, https://www.facebook.com/tienlam.nh.9, https://www.facebook.com/hungchodz99, https://www.facebook.com/MinhMan.4","Homepage":"berver.tech"}
+   console.log('/gai',req.ip)
+   res.end(JSON.stringify(response));
 })
 var server = app.listen(app.get("port"), function () {
 
